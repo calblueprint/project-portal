1f69e3e56178042834cd02e739c95c8c29b8624f
Joshua Primas <JoshuaPrimas@gmail.com>
2013-03-22 14:28:41 -0700

    created framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectPortal::Application.config.secret_token = 'a01f5faee05e5242d1270bcfac6fbaf22a793140df3b40eb9aeba343ddf614737d3b8767ae750bdb7f6fc82e505e79105549e94ecd5e1b2f04e0d24fdd769b69'
