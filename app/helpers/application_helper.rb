5fbae775068b3cbf003c2e92dd913814bd0c3153
bspolley <bspolley@gmail.com>
2013-03-25 21:17:12 -0700

    populating project card, added methods to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def url_with_http(url)
+    /^http/.match(url) ? url : "http://#{url}"
+  end
+  
+  def yes_no(bool)
+    bool ? "Yes" : "No"
+  end
 end
--- config/initializers/url_helper.rb
+++
@@ -1,3 +0,0 @@
-def url_with_http(url)
-  /^http/.match(url) ? url : "http://#{url}"
-end
