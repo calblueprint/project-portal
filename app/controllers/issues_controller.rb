3cb7ef0df7a8f951e45c2e7281a712afb6bf6980
Joshua Primas <JoshuaPrimas@gmail.com>
2013-03-27 18:07:12 -0700

    added form for new issues with error warnings

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,4 +1,8 @@
 class IssuesController < ApplicationController
+  def index
+		@title = "HOMEE"
+	end
+
   #displays a specfic issue	
 	def show
 		@issue = Issue.find(params[:id])
@@ -7,6 +11,22 @@ class IssuesController < ApplicationController
 	#redirects to page where you can specify data about a new issue
 	def new
 		@title = "Create an Issue"
-		#@issue = Event.new
+		@issue = Issue.new
+		#@issue.resolved = false
+	end
+
+	#actually creates a new event
+	def create
+		@issue = Issue.new(params[:id])
+		@issue.resolved = false
+		@issue.title = params[:issue][:title]
+		@issue.description = params[:issue][:description]
+
+	    if @issue.save
+	      flash[:notice] = "Issue Added"
+	      redirect_to(:action => 'show')
+	    else
+	      render action: "new"
+	    end
 	end
 end
