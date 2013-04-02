176d8e183c034991967f8c4d7fee3cef40f227b5
Helen <helen@helenweng.com>
2013-04-01 22:12:10 -0700

    admin, bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ProjectPortal::Application.routes.draw do
-  devise_for :users
+  get "home/index"
+
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
   resources :projects
   match 'projects/:id/user_edit' => 'projects#user_edit', :as => :user_edit_project
@@ -53,7 +55,7 @@ ProjectPortal::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
