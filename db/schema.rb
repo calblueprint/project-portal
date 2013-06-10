cb46579f9e3fccb2f3d6385641d381e0d64cdfc5
kgong <kevinkgong@gmail.com>
2013-06-10 14:22:25 -0700

    Front page styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609061859) do
+ActiveRecord::Schema.define(:version => 20130610062246) do
 
   create_table "clients", :force => true do |t|
     t.datetime "created_at",        :null => false
