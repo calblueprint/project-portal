6bf87fa6f248d17723f147ffa06ef4c168c0799e
kgong <kevinkgong@gmail.com>
2013-06-09 23:59:48 -0700

    Add upload picture ability to Client login

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ActiveRecord::Base
   attr_accessible :company_name, :company_site, :company_address, :nonprofit,
                   :five_01c3, :mission_statement, :contact_email,
-                  :contact_number
+                  :contact_number, :photo
   has_one :user, :as => :rolable
+  mount_uploader :photo, PhotoUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609212556) do
+ActiveRecord::Schema.define(:version => 20130610062246) do
 
   create_table "clients", :force => true do |t|
     t.datetime "created_at",        :null => false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130609212556) do
     t.string   "mission_statement"
     t.string   "contact_email"
     t.string   "contact_number"
+    t.string   "photo"
   end
 
   create_table "comments", :force => true do |t|
