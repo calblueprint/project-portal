f2e68bc5b388110646d7ffd5e76a63330728721b
kgong <kevinkgong@gmail.com>
2013-06-12 01:39:38 -0700

    Modify client dashboard cards including recent comments

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,7 @@
 module UserHelper
+
+  def get_comments(project, number)
+    @comments = Comment.order('created_at').find_all_by_commentable_id_and_commentable_type(project.id, 'Project').last(number)
+  end
+
 end
