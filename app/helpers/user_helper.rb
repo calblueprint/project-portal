1d041445dd27e3d5052b34e5c3e2b4055c066529
kgong <kevinkgong@gmail.com>
2013-08-04 16:26:38 -0700

    Convert some .erb to .haml

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,4 +4,30 @@ module UserHelper
     @comments = Comment.order('created_at').find_all_by_commentable_id_and_commentable_type(project.id, 'Project').last(number)
   end
 
+  def is_client?(id)
+  	user = User.find(id)
+  	if user.rolable_type == "Client"
+  		return true
+  	else
+  		return false
+  	end
+  end
+
+  def is_developer?(id)
+  	user = User.find(id)
+  	if user.rolable_type == "Developer"
+  		return true
+  	else
+  		return false
+  	end
+  end
+
+  def is_organization?(id)
+  	user = User.find(id)
+  	if user.rolable_type == "Organization"
+  		return true
+  	else
+  		return false
+  	end
+  end
 end
