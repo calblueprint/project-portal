502b98bb0b73c41113aa702c05e81ddf0224adec
callmemc <callmemc@gmail.com>
2013-08-13 22:54:19 -0700

    admin_dashboard route fix

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -60,12 +60,12 @@ class UserController < ApplicationController
     @user = User.find_by_email(@email)
     if @user and not @user.admin?
       @user.update_attributes(:admin=>true)
-      redirect_to user_admin_dashboard_path, notice: "#{@user.fname} #{@user.lname} is now an admin."
+      redirect_to admin_dashboard_path, notice: "#{@user.fname} #{@user.lname} is now an admin."
     elsif @user and @user.admin?
-      redirect_to user_admin_dashboard_path, notice: "#{@user.fname} #{@user.lname} is already an admin."
+      redirect_to admin_dashboard_path, notice: "#{@user.fname} #{@user.lname} is already an admin."
     else
       flash[:error] =  "#{@email} does not exist. Would you like to create a user?"
-      redirect_to user_admin_dashboard_path
+      redirect_to admin_dashboard_path
     end
   end
 
