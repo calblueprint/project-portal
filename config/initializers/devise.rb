38910ccda5be316b4956cf2ded9b1f4326a9a313
Sam Lau <samlau95@gmail.com>
2014-05-29 23:09:31 -0700

    Use ENV secret tokens

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,15 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # The secret key used by Devise. Devise uses this key to generate
+  # random tokens. Changing this key will render invalid all existing
+  # confirmation, reset password and unlock tokens in the database.
+  config.secret_key = if Rails.env.development? or Rails.env.test?
+    ('x' * 30)
+  else
+    ENV['DEVISE_SECRET_TOKEN']
+  end
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ProjectPortal::Application.config.secret_token = 'a01f5faee05e5242d1270bcfac6fbaf22a793140df3b40eb9aeba343ddf614737d3b8767ae750bdb7f6fc82e505e79105549e94ecd5e1b2f04e0d24fdd769b69'
+ProjectPortal::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30)
+else
+  ENV['SECRET_TOKEN']
+end
